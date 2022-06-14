d1b37511402e0ff64a08c6d5abcecccabbd1b94c
FedericoCapitani <federico.capitani@outlook.it>
2022-06-14 15:09:08 +0200

    add routes to home and locations

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<p>prova home</p>
+<a href="{{route('locations')}}">locations</a>
+@endsection
---
+++ resources/views/locations.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<p>prova locations</p>
+<a href="{{route('home')}}">home</a>
+@endsection
